30e9d4bbc255798288659ebe159ab510ff2e6bd2
冯天如 <fengtr@dustess.com>
2020-09-29 10:30:28 +0800

    feat: 🎸 import cubra

--- gogen/cmd/root.go
+++ gogen/cmd/root.go
@@ -1,7 +1,25 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"sync"
+
+	"github.com/spf13/cobra"
+)
 
 var (
-	rootCmd = &cobra.Command{}
+	rootCmd *cobra.Command
+	once    sync.Once
 )
+
+// NewCmd new cobra
+func NewCmd() *cobra.Command {
+	once.Do(func() {
+		rootCmd = &cobra.Command{
+			Use:   "gogen",
+			Short: "ChengFeng code generater artifact by fengtianru117",
+			Long:  "This command will generate code by customed golang struct",
+			RunE:  nil,
+		}
+	})
+	return rootCmd
+}
